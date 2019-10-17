b388224a4b9ca913de1002b139771b6bf355c899
JoaquinGomezCabido <joaquingomezcabido@gmail.com>
2019-10-17 15:07:44 +0100

    added serializer for room and fixed some other shit in the API functions

--- transchat/app/controllers/rooms_controller.rb
+++ transchat/app/controllers/rooms_controller.rb
@@ -4,5 +4,10 @@ class RoomsController < ApplicationController
         room = Room.create()
         render json: room
     end
+    
+    def show
+        rooms = User.find(params[:id]).rooms
+        render json: RoomSerializer.new(rooms)
+    end
 
 end
