407acf8b5aaf63d17252b5a2564a57ab7ed8e31c
JoaquinGomezCabido <joaquingomezcabido@gmail.com>
2019-10-18 10:23:58 +0100

    added redis gem

--- transchat/config/routes.rb
+++ transchat/config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :sessions, only: [:create, :show]
   resources :rooms, only: [:create, :show]
   resources :users, only: [:create, :show, :index]
+  mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
